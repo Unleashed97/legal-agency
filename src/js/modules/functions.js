fca77f7f07cdd71912178628ce832df267236f2a
Nichkov Roman <nichkovroman@gmail.com>
2022-12-26 18:07:49 +0300

    feat: add burger, adaptive header

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,1 +1,9 @@
+export const handleBurger = () => {
+    const burgerBtn = document.querySelector('.burger')
+    const mobileMenu = document.querySelector('.m-menu')
 
+    burgerBtn.addEventListener('click', () => {
+        burgerBtn.classList.toggle('active')
+        mobileMenu.classList.toggle('active')
+    })
+}
