026b5ec480128669f419f9d3bbea3a85a3cf2fb6
Nichkov Roman <nichkovroman@gmail.com>
2022-12-27 23:58:13 +0300

    fix: buttons, link scroll smooth,margins

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -6,4 +6,20 @@ export const handleBurger = () => {
         burgerBtn.classList.toggle('active')
         mobileMenu.classList.toggle('active')
     })
+
+    window.addEventListener('click', ({ target }) => {})
+}
+
+export const handleNavScroll = () => {
+    const burgerBtn = document.querySelector('.burger')
+    const mobileMenu = document.querySelector('.m-menu')
+
+    const navLinkList = document.querySelectorAll('.nav__link')
+
+    navLinkList.forEach((navLink) =>
+        navLink.addEventListener('click', () => {
+            burgerBtn.classList.remove('active')
+            mobileMenu.classList.remove('active')
+        }),
+    )
 }
